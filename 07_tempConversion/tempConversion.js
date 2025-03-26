62400be109d52712b96ee3f3d26464e7f9714afe
AndrejGjorevski <gorevskia9@gmail.com>
2025-03-26 16:45:12 +0100

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let temperatureInCelsious = (5 / 9) * (temperature - 32);
+  if (!Number.isInteger(temperatureInCelsious)) {
+    temperatureInCelsious = parseFloat(temperatureInCelsious.toFixed(1));
+  }
+
+  return temperatureInCelsious;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let temperatureInFahrenheit = (9 / 5) * temperature + 32;
+  if (!Number.isInteger(temperatureInFahrenheit)) {
+    temperatureInFahrenheit = parseFloat(temperatureInFahrenheit.toFixed(1));
+  }
+
+  return temperatureInFahrenheit;
 };
 
 // Do not edit below this line
