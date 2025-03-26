2c5b84f4745a7b2e2321b86f7dc67177db712e28
AndrejGjorevski <gorevskia9@gmail.com>
2025-03-26 16:18:57 +0100

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let resArr = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let shouldRemove = false;
+
+        for (let j = 0; j < args.length; j++) {
+            if (array[i] === args[j]) {
+                shouldRemove = true;
+                break;
+            }
+        }
+
+        if (shouldRemove) {
+            continue;
+        } else {
+            resArr.push(array[i]);
+        }
+    }
+
+    return resArr;
 };
 
 // Do not edit below this line
