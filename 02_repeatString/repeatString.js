d5f10ccb0235c36ba791152be6d0d669f4f57424
AndrejGjorevski <gorevskia9@gmail.com>
2025-03-26 15:16:38 +0100

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(inputString, num) {
+    let resultingString = "";
+    if (num === 0) {
+        return resultingString;
+    } else if (num < 0) {
+        resultingString = "ERROR";
+        return resultingString;
+    } else {
+        for (let i = 0; i < num; i++) {
+            resultingString += inputString;
+        }
 
+        return resultingString;
+    }
 };
 
 // Do not edit below this line
