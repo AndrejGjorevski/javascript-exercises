72feec9ab8c642f4ee4acbca1b00524b325386ae
AndrejGjorevski <gorevskia9@gmail.com>
2025-03-26 16:27:22 +0100

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let result = 0;
 
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        result = "ERROR";
+    } else {
+        if (start > end) {
+            for (let i = end; i <= start; i++) {
+                result += i;
+            }
+        } else {
+            for (let i = start; i <= end; i++) {
+                result += i;
+            }
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
